9601a365dd9ad455cc9d845d2ffcc3bb1c5ad115
Mike Hemberger <mike@thestizmedia.com>
2020-03-05 13:56:26 -0500

    main Mai_Grid class

--- lib/classes/class-mai-grid.php
+++ lib/classes/class-mai-grid.php
@@ -1,218 +1,274 @@
 <?php
 
 /**
- * Plugin Name:     Mai Grid
- * Plugin URI:      https://maitheme.com
- * Description:     An easy to use block to display page/post/cpt/category/tag/term entries with customizeable layouts.
- * Version:         0.1.0
- *
- * Author:          BizBudding, Mike Hemberger
- * Author URI:      https://bizbudding.com
+ * This should handle templates, sanitization, enqueing of files, etc., but nothing with ACF
+ * since ACF should only be used for the block. We need a shortcode and helper function as well,
+ * outside of ACF.
  */
+class Mai_Grid {
 
-// Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) exit;
+	protected $context;
+	protected $type;
+	protected $fields;
+	protected $keys;
+	protected $args;
+	protected $config;
 
-/**
- * Main Mai_Grid Class.
- *
- * @since 0.1.0
- */
-final class Mai_Grid {
-
-	/**
-	 * @var   Mai_Grid The one true Mai_Grid
-	 * @since 0.1.0
-	 */
-	private static $instance;
-
-	/**
-	 * Main Mai_Grid Instance.
-	 *
-	 * Insures that only one instance of Mai_Grid exists in memory at any one
-	 * time. Also prevents needing to define globals all over the place.
-	 *
-	 * @since   0.1.0
-	 * @static  var array $instance
-	 * @uses    Mai_Grid::setup_constants() Setup the constants needed.
-	 * @uses    Mai_Grid::includes() Include the required files.
-	 * @uses    Mai_Grid::hooks() Activate, deactivate, etc.
-	 * @see     Mai_Grid()
-	 * @return  object | Mai_Grid The one true Mai_Grid
-	 */
-	public static function instance() {
-		if ( ! isset( self::$instance ) ) {
-			// Setup the setup.
-			self::$instance = new Mai_Grid;
-			// Methods.
-			self::$instance->setup_constants();
-			self::$instance->includes();
-			self::$instance->hooks();
-		}
-		return self::$instance;
+	function __construct( $args ) {
+		$this->context = 'block';
+		$this->config  = new Mai_Entry_Settings( $this->context ); // TODO: Use dependency injection.
+		$this->type    = $this->config->type;
+		$this->fields  = $this->config->get_fields();
+		$this->keys    = $this->config->get_keys();
+		$this->args    = $this->get_args( $args );
 	}
 
-	/**
-	 * Throw error on object clone.
-	 *
-	 * The whole idea of the singleton design pattern is that there is a single
-	 * object therefore, we don't want the object to be cloned.
-	 *
-	 * @since   0.1.0
-	 * @access  protected
-	 * @return  void
-	 */
-	public function __clone() {
-		// Cloning instances of the class is forbidden.
-		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'textdomain' ), '1.0' );
+	function get_args( $args ) {
+
+		/**
+		 * Get defaults and parse args.
+		 * Skip tabs.
+		 * Skip if not the type/context we need.
+		 * Check for sub fields first.
+		 */
+		$defaults = [
+			'context' => $this->context,
+			'type'    => $this->type,  // post, term, user.
+			'class'   => '',
+		];
+		foreach ( $this->fields as $name => $field ) {
+			// Skip if not the context we want.
+			if ( ! ( isset( $field[ $args['context'] ] ) && $field[ $args['context'] ] ) ) {
+				continue;
+			}
+			// Skip if field type is a tab.
+			if ( 'tab' === $field['type'] ) {
+				continue;
+			}
+			// Skip if block and not the block we need.
+			if ( 'block' === $args['context'] ) {
+				/**
+				 * Skip if not in the grid we need.
+				 * Nested conditionals so we don't do in_array() for no reason if not a block.
+				 */
+				if ( ! in_array( sprintf( 'mai_%s_grid', $args['type'] ), $field['group'] ) ) {
+					continue;
+				}
+			}
+			// Add to our defaults.
+			$defaults[ $name ] = $field;
+		}
+
+		// Parse args.
+		$args = wp_parse_args( $args, $defaults );
+
+		// Sanitize.
+		foreach ( $args as $name => $value ) {
+			// Has sub fields.
+			if ( isset( $this->fields[ $name ]['acf']['sub_fields'] ) ) {
+				if ( $value ) {
+					$sub_values = [];
+					foreach ( $value as $index => $group ) {
+						foreach ( $group as $sub_name => $sub_value ) {
+							$field                             = $this->fields[ $name ]['acf']['sub_fields'][ $sub_name ];
+							$sub_values[ $index ][ $sub_name ] = $this->sanitize( $sub_value, $field['sanitize'] );
+						}
+					}
+					$args[ $name ] = $sub_values;
+				}
+			} // Standard field.
+			else {
+				// Get the sanitization function, as type/context aren't actual fields.
+				$sanitize      = isset( $this->fields[ $name ] ) ? $this->fields[ $name ]['sanitize'] : 'esc_html';
+				$args[ $name ] = $this->sanitize( $value, $sanitize );
+			}
+		}
+
+		return apply_filters( 'mai_grid_args', $args );
 	}
 
-	/**
-	 * Disable unserializing of the class.
-	 *
-	 * @since   0.1.0
-	 * @access  protected
-	 * @return  void
-	 */
-	public function __wakeup() {
-		// Unserializing instances of the class is forbidden.
-		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'textdomain' ), '1.0' );
-	}
+	function render() {
 
-	/**
-	 * Setup plugin constants.
-	 *
-	 * @access  private
-	 * @since   0.1.0
-	 * @return  void
-	 */
-	private function setup_constants() {
-
-		// Plugin version.
-		if ( ! defined( 'MAI_GRID_VERSION' ) ) {
-			define( 'MAI_GRID_VERSION', mai_get_version() );
-		}
-
-		// Plugin Folder Path.
-		if ( ! defined( 'MAI_GRID_PLUGIN_DIR' ) ) {
-			define( 'MAI_GRID_PLUGIN_DIR', mai_get_dir() );
-		}
-
-		// Plugin Folder URL.
-		if ( ! defined( 'MAI_GRID_PLUGIN_URL' ) ) {
-			define( 'MAI_GRID_PLUGIN_URL', mai_get_url() );
-		}
-
-		// Plugin Includes Path.
-		if ( ! defined( 'MAI_GRID_INCLUDES_DIR' ) ) {
-			define( 'MAI_GRID_INCLUDES_DIR', mai_get_dir() . '/lib/includes/' );
-		}
-
-		// Plugin Classes Path.
-		if ( ! defined( 'MAI_GRID_CLASSES_DIR' ) ) {
-			define( 'MAI_GRID_CLASSES_DIR', mai_get_dir() . '/lib/classes/' );
-		}
-
-		// Templates Path.
-		if ( ! defined( 'MAI_GRID_TEMPLATES_DIR' ) ) {
-			define( 'MAI_GRID_TEMPLATES_DIR', mai_get_dir() . '/lib/templates/' );
-		}
-
-		// Templates URL.
-		if ( ! defined( 'MAI_GRID_TEMPLATES_URL' ) ) {
-			define( 'MAI_GRID_TEMPLATES_URL', MAI_GRID_PLUGIN_URL . 'templates/' );
-		}
-
-		// Plugin Root File.
-		if ( ! defined( 'MAI_GRID_PLUGIN_FILE' ) ) {
-			define( 'MAI_GRID_PLUGIN_FILE', __FILE__ );
-		}
-
-		// Plugin Base Name
-		if ( ! defined( 'MAI_GRID_BASENAME' ) ) {
-			define( 'MAI_GRID_BASENAME', dirname( plugin_basename( __FILE__ ) ) );
-		}
-
-	}
-
-	/**
-	 * Include required files.
-	 *
-	 * @access  private
-	 * @since   0.1.0
-	 * @return  void
-	 */
-	private function includes() {
-		// Includes.
-
-		foreach ( glob( MAI_GRID_INCLUDES_DIR . '*.php' ) as $file ) { include $file; }
-		// Classes.
-		// foreach ( glob( MAI_GRID_CLASSES_DIR . '*.php' ) as $file ) { include $file; }
-		// include_once __DIR__ . '/classes/class-template-loader.php';
-
-		// TODO: Remove, now using autoloader.
-		// include_once MAI_GRID_CLASSES_DIR . 'class-mai-entry-settings.php';
-		// include_once MAI_GRID_CLASSES_DIR . 'class-mai-entry.php';
-		// include_once MAI_GRID_CLASSES_DIR . 'class-mai-grid-base.php';
-		// include_once MAI_GRID_CLASSES_DIR . 'class-mai-grid-blocks.php';
-
-		// include_once __DIR__ . '/classes/class-grid.php';
-		// include_once __DIR__ . '/classes/class-grid-block.php';
-		// include_once __DIR__ . '/classes/class-post-grid.php';
-		// include_once __DIR__ . '/classes/class-post-grid-block.php';
-		// include_once __DIR__ . '/includes/acf-sortable-checkbox/acf-sortable-checkbox.php';
-		// include_once __DIR__ . '/includes/acf-sortable-group/acf-sortable-group.php';
-	}
-
-	/**
-	 * Run the hooks.
-	 *
-	 * @since   0.1.0
-	 * @return  void
-	 */
-	public function hooks() {
-		add_action( 'admin_init',             array( $this, 'updater' ) );
-		add_filter( 'acf/settings/load_json', array( $this, 'load_json' ) );
-	}
-
-	/**
-	//  * Setup the updater.
-	 *
-	 * composer require yahnis-elsts/plugin-update-checker
-	 *
-	 * @uses    https://github.com/YahnisElsts/plugin-update-checker/
-	 *
-	 * @return  void
-	 */
-	public function updater() {
-
-		// Bail if current user cannot manage plugins.
-		if ( ! current_user_can( 'install_plugins' ) ) {
+		// Bail if not showing any elements.
+		if ( empty( $this->args['show'] ) ) {
 			return;
 		}
 
-		// Bail if plugin updater is not loaded.
-		if ( ! class_exists( 'Puc_v4_Factory' ) ) {
-			return;
+		// Enqueue scripts and styles.
+		$this->enqueue_assets();
+
+		// Grid specific classes.
+		$this->args['class'] = 'mai-grid ' . $this->args['class'];
+		$this->args['class'] = trim( $this->args['class'] );
+
+		// Open.
+		mai_do_entries_open( $this->args );
+
+		// Entries.
+		$this->do_grid_entries();
+
+		// Close.
+		mai_do_entries_close( $this->args );
+	}
+
+	function do_grid_entries() {
+
+		switch ( $this->args['type'] ) {
+			case 'post':
+				$show  = array_flip( $this->args['show'] );
+				$posts = new WP_Query( $this->get_post_query_args() );
+				if ( $posts->have_posts() ) {
+					while ( $posts->have_posts() ) : $posts->the_post();
+
+						global $post;
+						mai_do_entry( $post, $this->args );
+
+					endwhile;
+				} else {
+					// TODO.
+				}
+				wp_reset_postdata();
+				break;
+				break;
+			case 'term':
+				// TODO.
+				break;
+			case 'user':
+				// TODO.
+				break;
 		}
 
-		// Setup the updater.
-		// $updater = Puc_v4_Factory::buildUpdateChecker( 'https://github.com/maithemewp/mai-grid-entries/', __FILE__, 'mai-grid' );
+	}
+
+	function get_post_query_args() {
+
+		$query_args = [
+			'post_type'           => $this->args['post_type'],
+			'posts_per_page'      => $this->args['number'],
+			'post_status'         => 'publish',
+			'offset'              => absint( $this->args['offset'] ),
+			'ignore_sticky_posts' => true,
+		];
+
+		// Handle query_by.
+		switch ( $this->args['query_by'] ) {
+			case 'parent':
+				$query_args['post_parent__in'] = $this->args['post_parent__in'];
+				break;
+			case 'title':
+				// Empty array returns all posts, so we need to check for values.
+				if ( $this->args['post__in'] ) {
+					$query_args['post__in'] = $this->args['post__in'];
+				}
+				break;
+			case 'tax_meta':
+				$tax_query = $meta_query = [];
+				if ( $this->args['taxonomies'] ) {
+					foreach ( $this->args['taxonomies'] as $taxo ) {
+						// Skip if we don't have all the tax query args.
+						if ( ! ( $taxo['taxonomy'] && $taxo['taxonomy'] && $taxo['taxonomy'] ) ) {
+							continue;
+						}
+						// Set the value.
+						$tax_query[] = [
+							'taxonomy' => $taxo['taxonomy'],
+							'field'    => 'id',
+							'terms'    => $taxo['terms'],
+							'operator' => $taxo['operator'],
+						];
+					}
+					// If we have tax query values.
+					if ( $tax_query ) {
+						$query_args['tax_query'] = $tax_query;
+						if ( $this->args['taxonomies_relation'] ) {
+							$query_args['tax_query'][] = [
+								'relation' => $this->args['taxonomies_relation'],
+							];
+						}
+					}
+				}
+				// TODO: Add meta_query.
+				if ( $this->args['meta_keys'] ) {
+
+				}
+				break;
+		}
+
+		// Exclude entries.
+		if ( ( 'title' !== $this->args['query_by'] ) && $this->args['post__not_in'] ) {
+			$query_args['post__not_in'] = $this->args['post__not_in'];
+		}
+
+		// Orderby.
+		if ( $this->args['orderby'] ) {
+			$query_args['orderby'] = $this->args['orderby'];
+			if ( 'meta_value_num' === $this->args['orderby'] ) {
+				$query_args['meta_key'] = $this->args['orderby_meta_key'];
+			}
+		}
+
+		// Order.
+		if ( $this->args['order'] ) {
+			$query_args['order'] = $this->args['order'];
+		}
+
+		return apply_filters( 'mai_post_grid_query_args', $query_args );
 	}
 
 	/**
-	 * Add path to load acf json files.
+	 * Sanitize a value. Checks for null/array.
 	 *
-	 * @since   0.1.0
+	 * @param   string $value      The value to sanitize.
+	 * @param   string $function   The function to use for escaping.
+	 * @param   bool   $allow_null Wether to return or escape if the value is.
 	 *
-	 * @param   array  The existing acf-json paths.
-	 *
-	 * @return  array  The modified paths.
+	 * @return  mixed
 	 */
-	function load_json( $paths ) {
-		$paths[] = untrailingslashit( MAI_GRID_PLUGIN_DIR ) . '/acf-json';
-		return $paths;
+	function sanitize( $value, $function = 'esc_html', $allow_null = false ) {
+
+		// Return null if allowing null.
+		if ( is_null( $value ) && $allow_null ) {
+			return $value;
+		}
+
+		// If array, escape and return it.
+		if ( is_array( $value ) ) {
+			$escaped = [];
+			foreach ( $value as $index => $item ) {
+				if ( is_array( $item ) ) {
+					$escaped[ $index ] = $this->sanitize( $item, $function );
+				} else {
+					$item              = trim( $item );
+					$escaped[ $index ] = $function( $item );
+				}
+			}
+
+			return $escaped;
+		}
+
+		// Return single value.
+		$value   = trim( $value );
+		$escaped = $function( $value );
+
+		return $escaped;
+	}
+
+	function enqueue_assets() {
+
+		if ( is_admin() ) {
+
+			// Query JS.
+			switch ( $this->args['type'] ) {
+				case 'post':
+					wp_localize_script( mai_get_handle() . '-wp-query', 'maiGridWPQueryVars', [
+						'fields' => $this->fields,
+						'keys'   => $this->keys,
+					] );
+					break;
+				case 'term':
+					break;
+			}
+		}
 	}
 
 }
-
