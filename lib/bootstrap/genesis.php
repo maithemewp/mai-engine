6e89c2039b40edaa7a1d554cecdde850d2822ed3
Mike Hemberger <mike@thestizmedia.com>
2020-03-10 14:11:51 -0400

    filter genesis layout and use mai_site_layout()

--- lib/bootstrap/genesis.php
+++ lib/bootstrap/genesis.php
@@ -60,3 +60,25 @@ function mai_remove_genesis_theme_supports() {
  * @return array
  */
 add_filter( 'genesis_initial_layouts', '__return_empty_array' );
+
+/**
+ * Use mai engine layout.
+ *
+ * @since 0.1.0
+ *
+ * @return array
+ */
+add_filter( 'genesis_site_layout', 'mai_site_layout' );
+function mai_site_layout( $layout ) {
+	/**
+	 * Remove layout filter from Genesis Connect for WooCommerce.
+	 * Mai Engine handles this instead.
+	 *
+	 * TODO: Check if products are supported? Or do we do that in the config by default?
+	 */
+	remove_filter( 'genesis_pre_get_option_site_layout', 'genesiswooc_archive_layout' );
+
+	$site_layout = mai_get_site_layout();
+
+	return $site_layout ?: $layout;
+}
