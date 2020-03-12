42135c244ce10369762313136fe1ba30bc9bca35
Mike Hemberger <mike@thestizmedia.com>
2020-03-12 16:53:19 -0400

    header scroll JS to IntersectionObserver

--- assets/js/min/global.js
+++ assets/js/min/global.js
@@ -1,16 +1,23 @@
 
-
+/**
+ * @todo Check if
+ */
 var isTop = new IntersectionObserver( function( tracker ) {
-	var tracker = tracker[0];
-	var header  = document.querySelector( '.site-header' );
-	// var body    = document.getElementsByTagName( 'body' )[0];
+	var tracker   = tracker[0];
+	var body      = document.getElementsByTagName( 'body' )[0];
+	var header    = document.querySelector( '.site-header' );
+	var hasSticky = body.classList.contains( 'has-sticky-header' );
 	// tracker.intersectionRatio
 	if ( tracker.isIntersecting ) {
-		header.classList.remove( 'is-stuck' );
-		// body.classList.remove( 'header-stuck' );
+		if ( hasSticky ) {
+			header.classList.remove( 'is-stuck' );
+		}
+		header.classList.remove( 'is-top' );
 	} else {
-		header.classList.add( 'is-stuck' );
-		// body.classList.add( 'header-stuck' );
+		if ( hasSticky ) {
+			header.classList.add( 'is-stuck' );
+		}
+		header.classList.add( 'is-top' );
 	}
 
 }, { threshold: [0,1] });
