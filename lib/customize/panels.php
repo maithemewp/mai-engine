cd2588fc742a0617493d0792c6158779a4c8e38b
Mike Hemberger <mike@thestizmedia.com>
2020-03-12 16:54:11 -0400

    Site identity header width/spacing settings

--- lib/customize/panels.php
+++ lib/customize/panels.php
@@ -46,3 +46,114 @@ function mai_add_panels() {
 		);
 	}
 }
+
+
+function mai_header_settings_defaults_temp_function() {
+	return [
+		'logo_width_large'   => '180px',
+		'logo_width_small'   => '120px',
+		'logo_spacing_large' => [
+			'top'    => '36px',
+			'bottom' => '36px',
+		],
+		'logo_spacing_small' => [
+			'top'    => '16px',
+			'bottom' => '16px',
+		],
+	];
+}
+
+
+add_action( 'init', 'mai_header_customizer_settings' );
+/**
+ * Add header customizer settings.
+ *
+ * @return  void
+ */
+function mai_header_customizer_settings() {
+
+	// Bail if no Kirki.
+	if ( ! class_exists( 'Kirki' ) ) {
+		return;
+	}
+
+	$config_id = 'mai_header';
+
+	/**
+	 * Kirki Config.
+	 */
+	Kirki::add_config(
+		$config_id,
+		[
+			'capability'  => 'edit_theme_options',
+			'option_type' => 'option',
+			'option_name' => $config_id,
+		]
+	);
+
+	// Kirki::add_field( $config_id, [
+	// 	'type'        => 'custom',
+	// 	'settings'    => 'logo_heading',
+	// 	'label'       => esc_html__( 'Logo/Title Sizing', 'mai-engine' ),
+	// 	'section'     => 'title_tagline',
+	// 	'priority'    => 60,
+	// 	'default'     => '',
+	// ] );
+
+	$defaults = mai_header_settings_defaults_temp_function();
+
+	Kirki::add_field( $config_id, [
+		'type'        => 'text',
+		'settings'    => 'logo_width_large',
+		'label'       => esc_html__( 'Logo Width', 'mai-engine' ),
+		'section'     => 'title_tagline',
+		'priority'    => 60,
+		'default'     => $defaults['logo_width_large'],
+	] );
+
+	Kirki::add_field( $config_id, [
+		'type'        => 'dimensions',
+		'settings'    => 'logo_spacing_large',
+		'label'       => esc_html__( 'Logo Spacing', 'mai-engine' ),
+		'section'     => 'title_tagline',
+		'priority'    => 60,
+		'default'     => [
+			'top'    => $defaults['logo_spacing_large']['top'],
+			'bottom' => $defaults['logo_spacing_large']['bottom'],
+		],
+		'choices'     => [
+			'labels' => [
+				'top'    => esc_html__( 'Top', 'mai-engine' ),
+				'bottom' => esc_html__( 'Bottom', 'mai-engine' ),
+			],
+		],
+	] );
+
+	Kirki::add_field( $config_id, [
+		'type'        => 'text',
+		'settings'    => 'logo_width_small',
+		'label'       => esc_html__( 'Shrink/Mobile Logo Width', 'mai-engine' ),
+		'section'     => 'title_tagline',
+		'priority'    => 60,
+		'default'     => $defaults['logo_width_small'],
+	] );
+
+	Kirki::add_field( $config_id, [
+		'type'        => 'dimensions',
+		'settings'    => 'logo_spacing_small',
+		'label'       => esc_html__( 'Shrink/Mobile Logo Spacing', 'mai-engine' ),
+		'section'     => 'title_tagline',
+		'priority'    => 60,
+		'default'     => [
+			'top'    => $defaults['logo_spacing_small']['top'],
+			'bottom' => $defaults['logo_spacing_small']['bottom'],
+		],
+		'choices'     => [
+			'labels' => [
+				'top'    => esc_html__( 'Top', 'mai-engine' ),
+				'bottom' => esc_html__( 'Bottom', 'mai-engine' ),
+			],
+		],
+	] );
+
+}
