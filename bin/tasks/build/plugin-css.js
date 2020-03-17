35ad21ecec0885fff7bfc66c10bf1b2b26231a0a
seothemes <seothemeswp@gmail.com>
2020-03-17 11:10:42 +0800

    Remove rem replace for admin stylesheets

--- bin/tasks/build/plugin-css.js
+++ bin/tasks/build/plugin-css.js
@@ -1,25 +1,47 @@
 'use strict';
 
-const gulp = require( 'gulp' ),
-	config = require( '../../config' ),
-	plumber = require( 'gulp-plumber' ),
-	sourcemap = require( 'gulp-sourcemaps' ),
-	sass = require( 'gulp-sass' ),
-	normalize = require( 'node-normalize-scss' ).includePaths,
-	postcss = require( 'gulp-postcss' ),
-	mqpacker = require( 'css-mqpacker' ),
-	autoprefix = require( 'autoprefixer' ),
-	cssnano = require( 'cssnano' ),
-	pxtorem = require( 'postcss-pxtorem' ),
-	fs = require( 'fs' ),
-	sassVars = require('gulp-sass-vars'),
-	notify = require( 'gulp-notify' ),
-	map = require( 'lodash.map' ),
-	rename = require( 'gulp-rename' ),
-	combineSelectors = require( 'postcss-combine-duplicated-selectors' ),
-	discardDuplicates = require( 'postcss-discard-duplicates' );
+const gulp              = require( 'gulp' ),
+	  config            = require( '../../config' ),
+	  plumber           = require( 'gulp-plumber' ),
+	  sourcemap         = require( 'gulp-sourcemaps' ),
+	  sass              = require( 'gulp-sass' ),
+	  normalize         = require( 'node-normalize-scss' ).includePaths,
+	  postcss           = require( 'gulp-postcss' ),
+	  mqpacker          = require( 'css-mqpacker' ),
+	  autoprefix        = require( 'autoprefixer' ),
+	  cssnano           = require( 'cssnano' ),
+	  pxtorem           = require( 'postcss-pxtorem' ),
+	  fs                = require( 'fs' ),
+	  sassVars          = require( 'gulp-sass-vars' ),
+	  notify            = require( 'gulp-notify' ),
+	  map               = require( 'lodash.map' ),
+	  rename            = require( 'gulp-rename' ),
+	  combineSelectors  = require( 'postcss-combine-duplicated-selectors' ),
+	  discardDuplicates = require( 'postcss-discard-duplicates' );
 
 module.exports = function() {
+
+	const getPostProcessors = function( fileName ) {
+		const postProcessors = [
+			mqpacker( {
+				sort: true,
+			} ),
+			autoprefix(),
+			cssnano( config.css.cssnano ),
+			combineSelectors,
+			discardDuplicates,
+		];
+
+		if ( fileName !== 'advanced-custom-fields' && fileName !== 'kirki' ) {
+			postProcessors.push( pxtorem( {
+				root_value: config.css.basefontsize,
+				replace: config.css.remreplace,
+				media_query: config.css.remmediaquery,
+			} ) );
+		}
+
+		return postProcessors;
+	};
 
 	let plugins = function() {
 		return fs.readdirSync( './assets/scss/plugins/' );
@@ -37,31 +59,18 @@ module.exports = function() {
 			return './assets/scss/plugins/' + stylesheet + '/__index.scss';
 		};
 
-		let themeVars = function () {
-			return require('../../../config/_default/config.json');
+		let themeVars = function() {
+			return require( '../../../config/_default/config.json' );
 		};
 
 		return gulp.src( fileSrc() )
-			.pipe(sassVars(themeVars(), {verbose: false}))
+			.pipe( sassVars( themeVars(), { verbose: false } ) )
 			.pipe( plumber() )
 			.pipe( rename( stylesheet + '.min.scss' ) )
 			.pipe( sass.sync( {
 				outputStyle: 'compressed',
 			} ) )
-			.pipe( postcss( [
-				mqpacker( {
-					sort: true,
-				} ),
-				autoprefix(),
-				cssnano( config.css.cssnano ),
-				combineSelectors,
-				discardDuplicates,
-				pxtorem( {
-					root_value: config.css.basefontsize,
-					replace: config.css.remreplace,
-					media_query: config.css.remmediaquery,
-				} )
-			] ) )
+			.pipe( postcss( getPostProcessors( stylesheet ) ) )
 			.pipe( gulp.dest( './assets/css/plugins/' ) )
 			.pipe( notify( { message: config.messages.css } ) );
 	} );
