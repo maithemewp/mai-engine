952f63367a1e323d83c3ca233c20f0044b580565
seothemes <seothemeswp@gmail.com>
2020-04-17 20:46:39 +0800

    Remove empty settings pages

--- lib/admin/settings.php
+++ lib/admin/settings.php
@@ -41,31 +41,3 @@ function mai_update_database_version() {
 
 	update_option( 'mai_db_version', mai_get_plugin_data( 'db-version' ) );
 }
-
-if ( function_exists( 'acf_add_options_page' ) ) {
-	acf_add_options_page(
-		[
-			'page_title' => mai_get_name(),
-			'menu_title' => mai_get_name(),
-			'menu_slug'  => mai_get_handle(),
-			'capability' => 'edit_posts',
-			'redirect'   => false,
-		]
-	);
-
-	acf_add_options_sub_page(
-		[
-			'page_title'  => 'Addons',
-			'menu_title'  => 'Addons',
-			'parent_slug' => mai_get_handle(),
-		]
-	);
-
-	acf_add_options_sub_page(
-		[
-			'page_title'  => 'Courses',
-			'menu_title'  => 'Courses',
-			'parent_slug' => mai_get_handle(),
-		]
-	);
-}
