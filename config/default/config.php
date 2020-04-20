9c9a26b348ca86eda13f17bf9c558a0c0a9b9858
Mike Hemberger <mike@thestizmedia.com>
2020-04-20 15:43:34 -0400

    allow default theme to be used

---
+++ config/default/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Mai Engine.
+ *
+ * @package   BizBudding\MaiEngine
+ * @link      https://bizbudding.com
+ * @author    BizBudding
+ * @copyright Copyright © 2020 BizBudding
+ * @license   GPL-2.0-or-later
+ */
+
+// Prevent direct file access.
+defined( 'ABSPATH' ) || die;
+
+return [];
