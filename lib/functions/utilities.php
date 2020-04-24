633ec74d47d57b99498b74e1e6bba03957dbd61d
seothemes <seothemeswp@gmail.com>
2020-04-24 13:16:47 +0800

    Fix footer widget areas setting defaults

--- lib/functions/utilities.php
+++ lib/functions/utilities.php
@@ -689,28 +689,3 @@ function mai_get_editor_localized_data() {
 
 	return $data;
 }
-
-/**
- * Count number of widgets in a widget area.
- *
- * @since 0.1.0
- *
- * @param string $widget_area_id The widget area ID.
- *
- * @return int
- */
-function mai_get_widget_count( $widget_area_id ) {
-	/**
-	 * If loading from front page, consult $_wp_sidebars_widgets rather than options
-	 * to see if wp_convert_widget_settings() has made manipulations in memory.
-	 */
-	global $_wp_sidebars_widgets;
-	$_wp_sidebars_widgets = ! empty( $_wp_sidebars_widgets ) ? $_wp_sidebars_widgets : get_option( 'sidebars_widgets', [] );
-	if ( isset( $_wp_sidebars_widgets[ $widget_area_id ] ) ) {
-		$widget_count = count( $_wp_sidebars_widgets[ $widget_area_id ] );
-	} else {
-		$widget_count = 0;
-	}
-
-	return $widget_count;
-}
