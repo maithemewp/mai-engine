3708c87d2df55d8796bddf27e794171ffa32d226
Mike Hemberger <mike@thestizmedia.com>
2020-05-20 14:30:05 -0400

    Allow widget areas config to have default content

--- lib/structure/widget-areas.php
+++ lib/structure/widget-areas.php
@@ -25,16 +25,23 @@ function mai_widget_areas() {
 		$hook     = isset( $widget_area['location'] ) ? $widget_area['location'] : false;
 		$priority = isset( $widget_area['priority'] ) ? $widget_area['priority'] : 10;
 		$defaults = [
-			'before' => sprintf( '<div class="%s has-%s-widgets"><div class="wrap">', $id, mai_get_widget_count( $id ) ),
-			'after'  => '</div></div>',
+			'before'  => sprintf( '<div class="%s"><div class="wrap">', $id ),
+			'content' => isset( $widget_area['default'] ) && ! empty( $widget_area['default'] ) ? $widget_area['default'] : '',
+			'after'   => '</div></div>',
 		];
 		$args     = isset( $widget_area['args'] ) ? wp_parse_args( $widget_area['args'], $defaults ) : $defaults;
 
 		if ( $hook && ! mai_is_element_hidden( mai_convert_case( $id ) ) ) {
 			add_action(
 				$hook,
-				function () use ( $id, $args ) {
+				function () use ( $id, $args, $defaults ) {
 					genesis_widget_area( $id, $args );
+					if ( ! is_active_sidebar( $id ) && ! empty( $defaults['content'] ) ) {
+						$content = str_replace( ' ', '&nbsp;', $defaults['content'] );
+						echo $defaults['before'];
+						echo wp_kses_post( do_shortcode( $content ) );
+						echo $defaults['after'];
+					}
 				},
 				$priority
 			);
