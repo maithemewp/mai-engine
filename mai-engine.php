2a7dbe72482fb55b0bba8437bdc2cd094501fee0
Mike Hemberger <mike@thestizmedia.com>
2020-05-29 17:42:26 -0400

    v0.3.7

--- mai-engine.php
+++ mai-engine.php
@@ -10,7 +10,7 @@
  * Plugin URI:        https: //github.com/maithemewp/mai-engine/
  * GitHub Plugin URI: https: //github.com/maithemewp/mai-engine/
  * Description:       The required plugin to power Mai child themes.
- * Version:           0.3.6
+ * Version:           0.3.7
  * Author:            BizBudding Inc.
  * Author URI:        https: //bizbudding.com/
  * Text Domain:       mai-engine
