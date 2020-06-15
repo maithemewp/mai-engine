d555632a1596ff81425a62f531ac52608d48b7aa
seothemes <seothemeswp@gmail.com>
2020-06-15 12:09:37 +0800

    Begin work on mai success theme

--- config/success/config.php
+++ config/success/config.php
@@ -10,12 +10,62 @@
  */
 
 return [
-	'demos'        => [
-		// 'law'      => 9,
-		// 'news' => 10,
-		// 'personal' => 11,
+	'demos'         => [
+		// 'local'    => 3,
+		// 'coaching' => 4,
+		'agency' => 5,
 	],
-	'google-fonts' => [
-		'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap',
+	'google-fonts'  => [
+		'https://fonts.googleapis.com/css2?family=Hind&family=Montserrat:wght@700&display=swap',
+	],
+	'theme-support' => [
+		'add' => [
+			'sticky-header',
+			'transparent-header',
+		],
+	],
+	'widget-areas'   => [
+		'remove' => [
+			'before-header',
+			'header-left',
+			'header-right',
+			'before-footer',
+			'footer',
+			'footer-credits',
+		],
+	],
+	'template-parts' => [
+		[
+			'id'       => 'before-header',
+			'location' => 'genesis_before_header',
+			'default'  => '',
+		],
+		[
+			'id'       => 'header-left',
+			'location' => 'mai_header_left',
+			'default'  => '',
+		],
+		[
+			'id'       => 'header-right',
+			'location' => 'mai_header_right',
+			'default'  => '',
+		],
+		[
+			'id'       => 'before-footer',
+			'location' => 'genesis_footer',
+			'priority' => 5,
+			'default'  => '',
+		],
+		[
+			'id'       => 'footer',
+			'location' => 'genesis_footer',
+			'default'  => '',
+		],
+		[
+			'id'       => 'footer-credits',
+			'location' => 'genesis_footer',
+			'priority' => 12,
+			'default'  => '',
+		],
 	],
 ];
