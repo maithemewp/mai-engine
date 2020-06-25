55e5761aa7df35aa4c5b724024d1d63edb57817d
Mike Hemberger <mike@thestizmedia.com>
2020-06-25 15:50:25 -0400

    site layouts need nested values like old way
    And new layout/style.

--- lib/customize/site-layouts.php
+++ lib/customize/site-layouts.php
@@ -19,7 +19,13 @@ add_action( 'init', 'mai_site_layouts_customizer_settings' );
  */
 function mai_site_layouts_customizer_settings() {
 	$handle  = mai_get_handle();
-	$section = $handle . '-site-layouts';
+	$name    = 'site-layouts';
+	$section = sprintf( '%s-%s', $handle, $name  );
+	$options  = [
+		'default' => sprintf( '%s[%s][default]', $handle, $name  ),
+		'archive' => sprintf( '%s[%s][archive]', $handle, $name  ),
+		'single'  => sprintf( '%s[%s][single]', $handle, $name  ),
+	];
 
 	\Kirki::add_section(
 		$section,
@@ -32,93 +38,222 @@ function mai_site_layouts_customizer_settings() {
 	\Kirki::add_field(
 		$handle,
 		[
-			'type'     => 'checkbox',
-			'settings' => 'boxed-container',
-			'section'  => $section,
-			'label'    => __( 'Enable boxed site container', 'mai-engine' ),
-			'default'  => current_theme_supports( 'boxed-container' ),
+			'type'        => 'checkbox',
+			'settings'    => 'boxed-container',
+			'option_type' => 'option',
+			'option_name' => $options['default'],
+			'section'     => $section,
+			'label'       => __( 'Enable boxed site container', 'mai-engine' ),
+			'default'     => current_theme_supports( 'boxed-container' ),
 		]
 	);
 
 	\Kirki::add_field(
 		$handle,
 		[
-			'type'     => 'select',
-			'settings' => 'site',
-			'section'  => $section,
-			'label'    => __( 'Site Default', 'mai-engine' ),
-			'default'  => isset( $layouts['default']['site'] ) && ! empty( $layouts['default']['site'] ) ? $layouts['default']['site'] : 'standard-content',
-			'choices'  => mai_get_site_layout_choices(),
+			'type'        => 'custom',
+			'settings'    => 'defaults-layout-divider',
+			'option_type' => 'option',
+			'option_name' => $options['default'],
+			'section'     => $section,
+			'default'     => sprintf( '<h3>%s</h3>', esc_html__( 'Defaults', 'mai-engine' ) ),
 		]
 	);
 
 	\Kirki::add_field(
 		$handle,
 		[
-			'type'     => 'select',
-			'settings' => 'archive',
-			'section'  => $section,
-			'label'    => __( 'Content Archives', 'mai-engine' ),
-			'default'  => isset( $layouts['default']['archive'] ) && ! empty( $layouts['default']['archive'] ) ? $layouts['default']['archive'] : 'wide-content',
-			'choices'  => mai_get_site_layout_choices(),
+			'type'        => 'select',
+			'settings'    => 'site',
+			'option_type' => 'option',
+			'option_name' => $options['default'],
+			'section'     => $section,
+			'label'       => __( 'Site Default', 'mai-engine' ),
+			'default'     => isset( $layouts['default']['site'] ) && ! empty( $layouts['default']['site'] ) ? $layouts['default']['site']: 'standard-content',
+			'choices'     => mai_get_site_layout_choices(),
 		]
 	);
 
 	\Kirki::add_field(
 		$handle,
 		[
-			'type'     => 'select',
-			'settings' => 'single',
-			'section'  => $section,
-			'label'    => __( 'Single Content', 'mai-engine' ),
-			'default'  => isset( $layouts['default']['single'] ) && ! empty( $layouts['default']['single'] ) ? $layouts['default']['single'] : '',
-			'choices'  => mai_get_site_layout_choices(),
+			'type'        => 'select',
+			'settings'    => 'archive',
+			'option_type' => 'option',
+			'option_name' => $options['default'],
+			'section'     => $section,
+			'label'       => __( 'Content Archives', 'mai-engine' ),
+			'default'     => isset( $layouts['default']['archive'] ) && ! empty( $layouts['default']['archive'] ) ? $layouts['default']['archive']: 'wide-content',
+			'choices'     => mai_get_site_layout_choices(),
 		]
 	);
-
-	/*
-	 * Archive fields.
-	 */
 
 	\Kirki::add_field(
 		$handle,
 		[
-			'type'     => 'custom',
-			'settings' => 'archive-layout-divider',
-			'section'  => $section,
-			'default'  => '<hr>',
+			'type'        => 'select',
+			'settings'    => 'single',
+			'option_type' => 'option',
+			'option_name' => $options['default'],
+			'section'     => $section,
+			'label'       => __( 'Single Content', 'mai-engine' ),
+			'default'     => isset( $layouts['default']['single'] ) && ! empty( $layouts['default']['single'] ) ? $layouts['default']['single']: '',
+			'choices'     => mai_get_site_layout_choices(),
 		]
 	);
 
-	$layouts = mai_get_config( 'site-layouts' );
-	$archive = mai_get_content_type_choices( true );
-	$single  = mai_get_content_type_choices( false );
+	$layouts    = mai_get_config( 'site-layouts' );
+	$post_types = get_post_types( [ 'public' => true ], 'objects' );
+	unset( $post_types['attachment'] );
 
-	foreach ( $archive as $type => $label ) {
+	foreach ( $post_types as $name => $post_type ) {
+
 		\Kirki::add_field(
 			$handle,
 			[
-				'type'     => 'select',
-				'settings' => $type,
-				'section'  => $section,
-				'label'    => $label,
-				'default'  => isset( $layouts['archive'][ $type ] ) && ! empty( $layouts['archive'][ $type ] ) ? $layouts['archive'][ $type ] : '',
-				'choices'  => mai_get_site_layout_choices(),
+				'type'        => 'custom',
+				'settings'    => $name . '-layout-divider',
+				'option_type' => 'option',
+				'option_name' => $options['default'],
+				'section'     => $section,
+				'default'     => sprintf( '<h3>%s</h3>', $post_type->label ),
 			]
 		);
-	}
 
-	foreach ( $single as $type => $label ) {
 		\Kirki::add_field(
 			$handle,
 			[
-				'type'     => 'select',
-				'settings' => $type,
-				'section'  => $section,
-				'label'    => $label,
-				'default'  => isset( $layouts['single'][ $type ] ) && ! empty( $layouts['single'][ $type ] ) ? $layouts['single'][ $type ] : '',
-				'choices'  => mai_get_site_layout_choices(),
+				'type'        => 'select',
+				'settings'    => $name,
+				'option_type' => 'option',
+				'option_name' => $options['single'],
+				'section'     => $section,
+				'label'       => esc_html__( 'Single', 'mai-engine' ),
+				'default'     => isset( $layouts['single'][ $name ] ) && ! empty( $layouts['single'][ $name ] ) ? $layouts['single'][ $name ]: '',
+				'choices'     => mai_get_site_layout_choices(),
 			]
 		);
+
+		if ( 'post' === $name || $post_type->has_archive ) {
+
+			\Kirki::add_field(
+				$handle,
+				[
+					'type'        => 'select',
+					'settings'    => $name,
+					'option_type' => 'option',
+					'option_name' => $options['archive'],
+					'section'     => $section,
+					'label'       => esc_html__( 'Archive', 'mai-engine' ),
+					'default'     => isset( $layouts['archive'][ $name ] ) && ! empty( $layouts['archive'][ $name ] ) ? $layouts['archive'][ $name ]: '',
+					'choices'     => mai_get_site_layout_choices(),
+				]
+			);
+
+		}
+
+		$taxonomies = get_object_taxonomies( $name, 'objects' );
+
+		if ( $taxonomies ) {
+			// Get only public taxonomies.
+			$taxonomies = wp_list_filter( $taxonomies, [ 'public' => true ] );
+			// Remove taxonomies we don't want.
+			unset( $taxonomies['post_format'] );
+			unset( $taxonomies['product_shipping_class'] );
+			unset( $taxonomies['yst_prominent_words'] );
+		}
+
+		if ( $taxonomies ) {
+
+			foreach ( $taxonomies as $taxo_name => $taxonomy ) {
+
+				\Kirki::add_field(
+					$handle,
+					[
+						'type'        => 'select',
+						'settings'    => $taxo_name,
+						'option_type' => 'option',
+						'option_name' => $options['archive'],
+						'section'     => $section,
+						'label'       => $taxonomy->label,
+						'default'     => isset( $layouts['archive'][ $taxo_name ] ) && ! empty( $layouts['archive'][ $taxo_name ] ) ? $layouts['archive'][ $taxo_name ]: '',
+						'choices'     => mai_get_site_layout_choices(),
+					]
+				);
+			}
+		}
+
 	}
+
+	\Kirki::add_field(
+		$handle,
+		[
+			'type'        => 'custom',
+			'settings'    => 'misc-layout-divider',
+			'option_type' => 'option',
+			'option_name' => $options['default'],
+			'section'     => $section,
+			'default'     => sprintf( '<h3>%s</h3>', esc_html__( 'Miscellaneous', 'mai-engine' ) ),
+		]
+	);
+
+	\Kirki::add_field(
+		$handle,
+		[
+			'type'        => 'select',
+			'settings'    => 'search',
+			'option_type' => 'option',
+			'option_name' => $options['archive'],
+			'section'     => $section,
+			'label'       => esc_html__( 'Search Results', 'mai-engine' ),
+			'default'     => isset( $layouts['archive']['search'] ) && ! empty( $layouts['archive']['search'] ) ? $layouts['archive']['search']: '',
+			'choices'     => mai_get_site_layout_choices(),
+		]
+	);
+
+	\Kirki::add_field(
+		$handle,
+		[
+			'type'        => 'select',
+			'settings'    => 'author',
+			'option_type' => 'option',
+			'option_name' => $options['archive'],
+			'section'     => $section,
+			'label'       => esc_html__( 'Author Archives', 'mai-engine' ),
+			'default'     => isset( $layouts['archive']['author'] ) && ! empty( $layouts['archive']['author'] ) ? $layouts['archive']['author']: '',
+			'choices'     => mai_get_site_layout_choices(),
+		]
+	);
+
+	\Kirki::add_field(
+		$handle,
+		[
+			'type'        => 'select',
+			'settings'    => 'date',
+			'option_type' => 'option',
+			'option_name' => $options['archive'],
+			'section'     => $section,
+			'label'       => esc_html__( 'Date Archives', 'mai-engine' ),
+			'default'     => isset( $layouts['archive']['date'] ) && ! empty( $layouts['archive']['date'] ) ? $layouts['archive']['date']: '',
+			'choices'     => mai_get_site_layout_choices(),
+		]
+	);
+
+	\Kirki::add_field(
+		$handle,
+		[
+			'type'        => 'select',
+			'settings'    => '404-page',
+			'option_type' => 'option',
+			'option_name' => $options['single'],
+			'section'     => $section,
+			'label'       => esc_html__( '404', 'mai-engine' ),
+			'default'     => isset( $layouts['single']['404-page'] ) && ! empty( $layouts['single']['404-page'] ) ? $layouts['single']['404-page']: '',
+			'choices'     => mai_get_site_layout_choices(),
+		]
+	);
 }
+
+// add_action( 'genesis_before', function() {
+// 	vd( get_option( 'mai-engine' ) );
+// });
