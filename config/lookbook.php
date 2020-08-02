ed544fed478dc1f8ffe63489e667001cd5438000
seothemes <seothemeswp@gmail.com>
2020-08-02 21:19:12 +0800

    Add setting defaults to lookbook config

--- config/lookbook.php
+++ config/lookbook.php
@@ -14,15 +14,16 @@ return [
 		'default' => 53,
 	],
 	'global-styles' => [
-		'colors' => [
-			'link'      => '#ffb282',
+		'breakpoint' => 1360,
+		'colors'     => [
+			'link'      => '#ffe1ce',
 			'primary'   => '#000000',
 			'secondary' => '#f5f5f5',
 			'body'      => '#000000',
 			'heading'   => '#000000',
 			'alt'       => '#f5f5f5',
 		],
-		'fonts'  => [
+		'fonts'      => [
 			'body'    => 'Lato:400',
 			'heading' => 'Playfair Display:400',
 			'menu'    => 'Playfair Display:700',
@@ -31,6 +32,44 @@ return [
 	'theme-support' => [
 		'add' => [
 			'sticky-header',
+		],
+	],
+	'settings'      => [
+		'logo'             => [
+			'show-tagline' => false,
+			'logo-spacing' => [
+				'desktop' => '20px',
+			],
+		],
+		'site-layout'      => [
+			'site'    => 'wide-content',
+			'archive' => 'wide-content',
+			'search'  => 'wide-content',
+			'single'  => 'wide-content',
+		],
+		'single-content'   => [
+			'image_orientation' => 'custom',
+			'image_size'        => 'cover',
+		],
+		'content-archives' => [
+			'show'              => [
+				'image',
+				'genesis_entry_header',
+				'header_meta',
+				'title',
+				'excerpt',
+				'genesis_before_entry_content',
+				'content',
+				'genesis_entry_content',
+				'genesis_after_entry_content',
+				'footer_meta',
+				'genesis_entry_footer',
+			],
+			'image_orientation' => 'custom',
+			'image_size'        => 'cover',
+			'title_size'        => 'xxxxl',
+			'boxed'             => false,
+			'columns'           => '1',
 		],
 	],
 ];
