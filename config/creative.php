5ad00c041094ef13492b8afaf6eb899e9faabe88
seothemes <seothemeswp@gmail.com>
2020-08-11 13:28:07 +0800

    Add creative config and styles

--- config/creative.php
+++ config/creative.php
@@ -9,4 +9,43 @@
  * @license   GPL-2.0-or-later
  */
 
-return [];
+return [
+	'global-styles' => [
+		'colors' => [
+			'alt'       => '#f5f5f5',
+			'body'      => '#4e4e4e',
+			'heading'   => '#2c2c2c',
+			'primary'   => '#2c2c2c',
+			'secondary' => '#e6e7e9',
+			'link'      => '#a6a59b',
+		],
+		'fonts'  => [
+			'body'    => 'Open Sans:400',
+			'heading' => 'Montserrat:600,500',
+		],
+	],
+	'image-sizes'   => [
+		'add'    => [
+			'cover'  => [ 1600, 900, true ],
+			'square' => '1:1',
+			'tiny'   => [ 80, 80, true ],
+		],
+		'remove' => [],
+	],
+	'settings'      => [
+		'logo'             => [
+			'width' => [
+				'desktop' => '300px',
+			],
+		],
+		'site-layout'      => [
+			'default' => [
+				'site' => 'content-sidebar',
+			],
+		],
+		'content-archives' => [
+			'columns' => '1',
+			'boxed'   => false,
+		],
+	],
+];
