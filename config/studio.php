553a12c95dcbeafd1b99db45a16ef21dcc3de566
Mike Hemberger <mike@thestizmedia.com>
2020-09-17 12:04:16 -0400

    add mai studio config

---
+++ config/studio.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Mai Engine.
+ *
+ * @package   BizBudding\MaiEngine
+ * @link      https://bizbudding.com
+ * @author    BizBudding
+ * @copyright Copyright © 2020 BizBudding
+ * @license   GPL-2.0-or-later
+ */
+
+return [
+	'demos'         => [
+		'agency'  => 52,
+	],
+	// 'global-styles' => [
+	// 	'colors' => [
+	// 		'alt'       => '#f7f8fa',
+	// 		'link'      => '#7b51ff',
+	// 		'primary'   => '#7b51ff',
+	// 		'secondary' => '#8f98a3',
+	// 		'heading'   => '#4b657e',
+	// 		'body'      => '#5f749e',
+	// 	],
+	// 	'fonts'  => [
+	// 		'body'    => 'Karla:400',
+	// 		'heading' => 'Karla:700',
+	// 	],
+	// ],
+	// 'theme-support' => [
+	// 	'add' => [
+	// 		'transparent-header',
+	// 	],
+	// ],
+	// 'settings'      => [
+	// 	'page-header' => [
+	// 		'archive'                 => '*',
+	// 		'single'                  => '*',
+	// 		'background-color'        => 'primary',
+	// 		'text-color'              => 'light',
+	// 		'divider'                 => 'curve',
+	// 		'divider-height'          => 'sm',
+	// 		'divider-color'           => 'white',
+	// 		'divider-flip-horizontal' => false,
+	// 	],
+	// ],
+	// 'plugins'       => [
+	// 	[
+	// 		'name'  => 'Genesis eNews Extended',
+	// 		'slug'  => 'genesis-enews-extended/plugin.php',
+	// 		'uri'   => 'https://wordpress.org/plugins/genesis-enews-extended/',
+	// 		'demos' => [ 'agency', 'podcast' ],
+	// 	],
+	// ],
+];
