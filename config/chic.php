4c3b3be1d0ad8d350128248e8e014215e3198c86
Mike Hemberger <mike@thestizmedia.com>
2020-09-22 00:35:39 -0400

    get chic and inspire started

--- config/chic.php
+++ config/chic.php
@@ -9,4 +9,45 @@
  * @license   GPL-2.0-or-later
  */
 
-return [];
+return [
+	'demos'            => [
+		// 'default' => 51,
+	],
+	'global-styles'    => [
+		'colors' => [
+			'link'           => '#b0cdbd',
+			'primary'        => '#b0cdbd',
+			'secondary'      => '#f5f5f5',
+			'heading'        => '#173821',
+			'body'           => '#173821',
+			'alt'            => '#f5f5f5',
+			'custom-color-1' => '#545454',
+		],
+		'fonts'  => [
+			'body'        => 'Lato:400',
+			'heading'     => 'Lato:700',
+			'entry-title' => 'Playfair Display:400',
+		],
+	],
+	'image-sizes'         => [
+		'add' => [
+			'landscape' => '16:9',
+			'square'    => '1:1',
+		],
+	],
+	'custom-functions' => function() {
+		add_action( 'init', function() {
+			/**
+			 * Register block style
+			 */
+			register_block_style(
+				'core/heading',
+				array(
+					'name'         => 'boxed-heading',
+					'label'        => __( 'Boxed Heading', 'mai-engine' ),
+					// 'style_handle' => 'prefix-stylesheet',
+				)
+			);
+		});
+	},
+];
---
+++ config/inspire.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Mai Engine.
+ *
+ * @package   BizBudding\MaiEngine
+ * @link      https://bizbudding.com
+ * @author    BizBudding
+ * @copyright Copyright © 2020 BizBudding
+ * @license   GPL-2.0-or-later
+ */
+
+return [
+	'demos'            => [
+		// 'default' => 51,
+	],
+	'global-styles'    => [
+		'colors' => [
+			// 'link'           => '#b0cdbd',
+			'primary'        => '#002627',
+			'secondary'      => '#894b32',
+			'heading'        => '#042525',
+			'body'           => '#252323',
+			// 'alt'            => '#f5f5f5',
+			// 'custom-color-1' => '#545454',
+		],
+		'fonts'  => [
+			'body'        => 'Lato:400',
+			'heading'     => 'Playfair Display:900',
+			// 'entry-title' => 'Playfair Display:400',
+		],
+	],
+	'image-sizes'         => [
+		'add' => [
+			// 'landscape' => '16:9',
+			'square'    => '1:1',
+		],
+	],
+	'custom-functions' => function() {
+		/**
+		 * Add custom body class.
+		 *
+		 * @param   array  The existing body classes.
+		 *
+		 * @return  array  Modified classes.
+		 */
+		add_filter( 'body_class', function( $classes ) {
+			$classes[] = 'has-dark-header';
+			return $classes;
+		});
+	},
+];
