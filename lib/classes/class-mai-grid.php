5f05210d8a6afcea0292228d03ab85dcc249b253
Mike Hemberger <mike@thestizmedia.com>
2020-10-14 11:40:05 -0400

    Fix error when exclude_displayed in Mai Post Grid

--- lib/classes/class-mai-grid.php
+++ lib/classes/class-mai-grid.php
@@ -437,8 +437,7 @@ class Mai_Grid {
 		// Exclude displayed.
 		if ( $this->args['excludes'] && in_array( 'exclude_displayed', $this->args['excludes'] ) && ! empty( self::$existing_post_ids ) ) {
 			if ( isset( $query_args['post__not_in'] ) ) {
-				$query_args['post__not_in'] = array_push( $query_args['post__not_in'], self::$existing_post_ids );
-
+				$query_args['post__not_in'] = array_merge( $query_args['post__not_in'], self::$existing_post_ids );
 			} else {
 				$query_args['post__not_in'] = self::$existing_post_ids;
 			}
