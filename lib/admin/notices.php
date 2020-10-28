731754f2447f0f994cdd284c8adc6a41c059c3c7
Mike Hemberger <mike@thestizmedia.com>
2020-10-28 16:00:01 -0400

    Add ability to display admin notices from query args

---
+++ lib/admin/notices.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Mai Engine.
+ *
+ * @package   BizBudding\MaiEngine
+ * @link      https://bizbudding.com
+ * @author    BizBudding
+ * @copyright Copyright © 2020 BizBudding
+ * @license   GPL-2.0-or-later
+ */
+
+add_action( 'admin_notices', 'mai_maybe_display_admin_notice', 20 );
+/**
+ * Displays notices from a query args.
+ *
+ * @since TBD
+ *
+ * @return void
+ */
+function mai_maybe_display_admin_notice() {
+	$notice = filter_input( INPUT_GET, 'mai_notice', FILTER_SANITIZE_STRING );
+
+	if ( ! $notice ) {
+		return;
+	}
+
+	$type = filter_input( INPUT_GET, 'mai_type', FILTER_SANITIZE_STRING );
+	$type = $type ?: 'success';
+
+	printf( '<div class="notice notice-%s">%s</div>', sanitize_html_class( $type ), wpautop( $notice ) );
+}
