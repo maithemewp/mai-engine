379fdb7ca7af16649ab225bccb8c4a8ccd1cfd5d
Mike Hemberger <mike@thestizmedia.com>
2020-11-10 10:16:03 -0500

    remove unused filter on widgets

--- lib/structure/single.php
+++ lib/structure/single.php
@@ -18,24 +18,6 @@ add_filter( 'edit_post_link', '__return_empty_string' );
 // Remove author 'says' text.
 add_filter( 'comment_author_says_text', '__return_empty_string' );
 
-add_filter( 'genesis_markup_entry-title_content', 'mai_feature_posts_widget_entry_title_link' );
-/**
- * Description of expected behavior.
- *
- * @since 0.1.0
- *
- * @param string $default Default string.
- *
- * @return string
- */
-function mai_feature_posts_widget_entry_title_link( $default ) {
-	$permalink = get_permalink();
-	$search    = sprintf( '<a href="%s">', $permalink );
-	$replace   = sprintf( '<a href="%s" class="entry-title-link">', $permalink );
-
-	return str_replace( $search, $replace, $default );
-}
-
 add_filter( 'comment_reply_link', 'mai_comment_reply_button_class' );
 /**
  * Add comment reply button classes.
