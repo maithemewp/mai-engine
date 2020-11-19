2e34b36463296062fa0e407cbc7deb2d72bacf60
Mike Hemberger <mike@thestizmedia.com>
2020-11-19 13:41:06 -0500

    Don’t show cart_total if empty cart

--- lib/functions/plugins.php
+++ lib/functions/plugins.php
@@ -358,7 +358,9 @@ function mai_get_cart_total() {
 	if ( ! $cart ) {
 		return;
 	}
-	return sprintf( '<span class="mai-cart-total-wrap is-circle"><span class="mai-cart-total">%s</span></span>',
-		WC()->cart->get_cart_contents_count()
-	);
+	$total = WC()->cart->get_cart_contents_count();
+	if ( ! $total ) {
+		return;
+	}
+	return sprintf( '<span class="mai-cart-total-wrap is-circle"><span class="mai-cart-total">%s</span></span>', $total );
 }
