e1c965ba8aebcef92a0ed2ed64d36ec863ae8d1e
Mike Hemberger <mike@thestizmedia.com>
2020-12-04 17:39:20 -0500

    Add plugins admin menu

--- lib/admin/settings.php
+++ lib/admin/settings.php
@@ -31,6 +31,17 @@ function mai_admin_menu_pages() {
 		59
 	);
 
+	// Changes first menu name. Otherwise above has Mai Theme as the first child too.
+	add_submenu_page(
+		'mai-theme',
+		esc_html__( 'Plugins', 'mai-engine' ),
+		esc_html__( 'Plugins', 'mai-engine' ),
+		'edit_posts',
+		'mai-theme',
+		'',
+		null
+	);
+
 	add_submenu_page(
 		'mai-theme',
 		esc_html__( 'Template Parts', 'mai-engine' ),
@@ -70,28 +81,82 @@ function mai_admin_menu_pages() {
  * @return void
  */
 function mai_render_admin_menu_page() {
-	global $title;
-
 	echo '<div class="wrap">';
-	echo "<h1>$title</h1>";
+		printf( '<h1>%s</h1>', __( 'Mai Theme', 'mai-engine' ) );
 
-	$theme = mai_get_active_theme();
-	$theme = 'default' === $theme ? __( 'Theme', 'mai-engine' ) : mai_convert_case( $theme, 'title' );
+		printf( '<div class="notice notice-success"><p>%s</p></div>', __( 'Supercharge your website with our custom add-on plugins!', 'mai-engine' ) );
 
-	echo sprintf(
-		'<p>%s %s.</p><h2>%s</h2>',
-		__( 'Hi! Thanks for choosing Mai', 'mai-engine' ),
-		$theme,
-		__( 'Quick Links', 'mai-engine' )
-	);
+		printf( '<h2 style="margin-top:40px;">%s</h2>', __( 'Add-on Plugins', 'mai-engine' ) );
 
-	echo '<ul>';
+		$image_base = sprintf( '%s/assets/img', untrailingslashit( mai_get_url() ) );
+		$plugins    = [
+			[
+				'link'  => 'https://bizbudding.com/products/mai-favorites/',
+				'image' => sprintf( '%s/mai-favorites.jpg', $image_base ),
+				'title' => __( 'Mai Favorites', 'mai-engine' ),
+				'desc'  => __( 'Give your visitors a way to easily browse collections of your favorite things, whether that be affiliate products, recommendations, or anything you want. Your favorites are easily displayed on a page via Mai Post Grid block so you can change the layout, organize by Favorite Category, etc.', 'mai-engine' ),
+				'hide'  => class_exists( 'Mai_Favorites_Setup' ),
+			],
+			[
+				'link'  => 'https://bizbudding.com/products/mai-testimonials/',
+				'image' => sprintf( '%s/mai-testimonials.jpg', $image_base ),
+				'title' => __( 'Mai Testimonials', 'mai-engine' ),
+				'desc'  => __( 'Show off all the great things your customers/clients have to say about you. Manage your testimonials in one place, and easily hide them with Mai Post Grid block.', 'mai-engine' ),
+				'hide'  => class_exists( 'Mai_Testimonials' ),
+			],
+			[
+				'link'  => 'https://bizbudding.com/products/mai-ads-extra-content/',
+				'image' => sprintf( '%s/mai-ads.jpg', $image_base ),
+				'title' => __( 'Mai Ads & Extra Content', 'mai-engine' ),
+				'desc'  => __( 'Boost your sales with display ads or content across a multitude of areas on your site all from one simple-to-manage spot. Pairs perfectly with the lead generation tool, ConvertFlow, for embedding targeted, dynamic CTAs.', 'mai-engine' ),
+				'hide'  => class_exists( 'Mai_AEC' ),
+			],
+			[
+				'link'  => 'https://bizbudding.com/products/mai-effects/',
+				'image' => sprintf( '%s/mai-effects.jpg', $image_base ),
+				'title' => __( 'Mai Effects', 'mai-engine' ),
+				'desc'  => __( 'Coming soon! Add stand-out fadein animation effects, once only possible with developer intervention! Make your page header and sections pop.', 'mai-engine' ),
+				'hide'  => class_exists( 'Mai_Effects' ),
+			],
+			[
+				'link'  => 'https://bizbudding.com/products/mai-theme-plugin-pack/',
+				'image' => sprintf( '%s/mai-plugin-pack.jpg', $image_base ),
+				'title' => __( 'Mai Theme Plugin Pack', 'mai-engine' ),
+				'desc'  => __( 'Get all of our add-on plugins at a big discounted rate.', 'mai-engine' ),
+				'hide'  => false,
+			],
+		];
 
-	echo '<li>· <a href="https://support.bizbudding.com/" target="_blank" rel="noopener nofollow">' . __( 'Support', 'mai-engine' ) . '</a></li>';
-	echo '<li>· <a href="https://bizbudding.com/" target="_blank" rel="noopener nofollow">' . __( 'BizBudding', 'mai-engine' ) . '</a></li>';
-	echo '<li>· <a href="https://demo.bizbudding.com/" target="_blank" rel="noopener nofollow">' . __( 'Theme Demos', 'mai-engine' ) . '</a></li>';
+		echo '<ul class="mai-plugins">';
 
-	echo '<ul>';
+			foreach ( $plugins as $plugin ) {
+				if ( $plugin['hide'] ) {
+					continue;
+				}
+
+				echo '<li class="mai-plugin">';
+					echo '<a class="mai-plugin-image-link" href="%s">';
+						printf( '<img class="mai-plugin-image" src="%s" alt="%s %s">', $plugin['image'], $plugin['title'], __( 'product image', 'mai-theme' ) );
+					echo '</a>';
+					echo '<div class="mai-plugin-content">';
+						echo '<h3 class="mai-plugin-title">';
+							printf( '<a class="mai-plugin-title-link" href="%s" target="_blank" rel="noopener nofollow">', $plugin['link'] );
+								echo $plugin['title'];
+							echo '</a>';
+						echo '</h3>';
+						echo '<p class="mai-plugin-description">';
+							echo $plugin['desc'];
+						echo '</p>';
+						echo '<p class="mai-plugin-more-link-wrap">';
+							printf( '<a class="mai-plugin-more-link button button-primary" href="%s" target="_blank" rel="noopener nofollow">', $plugin['link'] );
+								echo __( 'Learn More', 'mai-engine' );
+							echo '</a>';
+						echo '</p>';
+					echo '</div>';
+				echo '</li>';
+			}
+
+		echo '<ul>';
 
 	echo '</div>';
 }
