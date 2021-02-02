984373f7c5486a873e6c8c114d3b473767eb0653
Mike Hemberger <mike@thestizmedia.com>
2021-02-02 12:52:41 -0500

    update_post_thumbnail_cache in all loops

--- lib/functions/performance.php
+++ lib/functions/performance.php
@@ -86,3 +86,34 @@ function mai_remove_recent_comments_style() {
 
 	remove_action( 'wp_head', [ $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ] );
 }
+
+/**
+ * Prime post thumbnail cache.
+ *
+ * Automatically primes the cache for the main loop on the blog page,
+ * archives, and instances of Mai Post Grid.
+ *
+ * The cache can be activated for additional queries using the 'mai_cache_featured_images' filter.
+ *
+ * Custom queries can also pass a 'mai_cache_featured_images' flag to WP_Query via the
+ * query args to prime the cache.
+ *
+ * Props Brady Vercher.
+ *
+ * @since TBD
+ *
+ * @param array    $posts    List of posts in the query.
+ * @param WP_Query $wp_query WP Query object.
+ *
+ * @return array
+ */
+add_action( 'the_posts', 'mai_prime_featured_images_cache', 10, 2 );
+function mai_prime_featured_images_cache( $posts, $wp_query ) {
+	if ( ! ( $wp_query->is_main_query() && mai_is_type_archive() ) ) {
+		return $posts;
+	}
+
+	update_post_thumbnail_cache( $wp_query );
+
+	return $posts;
+}
