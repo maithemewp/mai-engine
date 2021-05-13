c0ce651f326584487480ced681d53f2411a6b26e
Mike Hemberger <mike@thestizmedia.com>
2021-05-13 13:03:01 -0400

    delete transient even when saving revisions
    we still hit the bug related to this sometimes. no harm deleting it all the time.

--- lib/init.php
+++ lib/init.php
@@ -262,18 +262,6 @@ function mai_load_default_favicon( $favicon ) {
  */
 add_action( 'save_post_mai_template_part', 'mai_save_template_part_delete_transient', 20, 3 );
 function mai_save_template_part_delete_transient( $post_id, $post, $update ) {
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-		return;
-	}
-
-	if ( wp_is_post_autosave( $post_id ) ) {
-		return;
-	}
-
-	if ( wp_is_post_revision( $post_id ) ) {
-		return;
-	}
-
 	delete_transient( 'mai_template_parts' );
 }
 
