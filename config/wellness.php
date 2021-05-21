d5819545f9e15aad5e5e3300492ae2a9a107edec
Mike Hemberger <mike@thestizmedia.com>
2021-05-21 15:26:44 -0400

    start wellness theme

---
+++ config/wellness.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Mai Engine.
+ *
+ * @package   BizBudding\MaiEngine
+ * @link      https://bizbudding.com
+ * @author    BizBudding
+ * @copyright Copyright © 2020 BizBudding
+ * @license   GPL-2.0-or-later
+ */
+
+return [
+	// 'demos'         => [
+		// 'local'    => 3,
+		// 'coaching' => 4,
+	// 	'business' => 5,
+	// ],
+	'global-styles' => [
+		'colors' => [
+		// 'black'      => '#000000',
+		// 'white'      => '#ffffff',
+		// 'header'     => '#ffffff', // Site header background.
+		// 'background' => '#ffffff', // Body background.
+		// 'alt'        => '#f8f9fa', // Background alt.
+		'body'       => '#404040', // Body text color.
+		'heading'    => '#121212', // Heading text color.
+		// 'link'       => '#c66cdf', // Link color.
+		'link'       => '#298f43', // Link color.
+		'primary'    => '#298f43', // Button primary background color.
+		// 'secondary'  => '#6c747d', // Button secondary background color.
+		],
+		'fonts'  => [
+			// 'body'    => 'Karma:400',
+			'body'    => 'Merriweather:400',
+			'heading' => 'Noto Sans:700',
+			'menu'    => 'Noto Sans:400',
+		],
+	],
+	'theme-support' => [
+		'add' => [
+			// 'transparent-header',
+			'sticky-header',
+		],
+	],
+];
