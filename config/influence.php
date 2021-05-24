b523c7468518a78339473849d6a5e71c6776eb4a
Mike Hemberger <mike@thestizmedia.com>
2021-05-24 16:13:08 -0400

    influence archives config

--- config/influence.php
+++ config/influence.php
@@ -38,7 +38,25 @@ return [
 	'settings' => [
 		'content-archives' => [
 			'post' => [
-				'columns' => 1,
+				'show'        => [
+					'header_meta',
+					'title',
+					'footer_meta',
+					'image',
+					'genesis_entry_header',
+					'genesis_before_entry_content',
+					'excerpt',
+					'genesis_entry_content',
+					'more_link',
+					'genesis_after_entry_content',
+					'genesis_entry_footer',
+				],
+				'boxed'       => false,
+				'columns'     => 1,
+				'footer_meta' => '[mai_icon icon="location-arrow" style="solid" size=".5em" margin_right="4px" display="inline-flex"][post_categories before="Location: "]',
+				'header_meta' => '[post_date]',
+				'align_text'  => 'center',
+				'row_gap'     => 'xxxl',
 			],
 		],
 		'site-layouts' => [
