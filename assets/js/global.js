3b20a0e51dea2d2c24afc259e32bcbaec68025e4
Mike Hemberger <mike@thestizmedia.com>
2021-09-28 11:47:53 -0400

    refactor menu and search toggle js
    simpler events/functions and now they both close each other if open when opening the other

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,66 +1,63 @@
 ( function() {
 	var root           = document.documentElement;
+	var body           = document.getElementsByTagName( 'body' )[ 0 ];
 	var scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
 	var searchToggles  = document.querySelectorAll( '.search-toggle' );
 
-	var toggleSearchForm = function( event ) {
-		var parent = this.closest( '.search-icon-form' );
+	// Sets scrollbar width on page load.
+	root.style.setProperty( '--scrollbar-width', scrollBarWidth + 'px' );
+
+	var toggleSearchFormEvent = function( event ) {
+		var element = event.target.closest( '.search-icon-form' );
+
+		if ( ! element ) {
+			return;
+		}
+
+		toggleSearchForm( element );
+	}
+
+	var toggleSearchForm = function( element ) {
+		var parent = element.closest( '.search-icon-form' );
 
 		if ( ! parent ) {
 			return;
 		}
 
-		var target = event.target;
 		var form   = parent.querySelector( '.search-form' );
 		var toggle = parent.querySelector( '.search-toggle' );
 		var input  = parent.querySelector( '.search-form-input' );
 
-		var hideSearchForm = function( event ) {
-			if ( ! event.target.closest( '.search-icon-form' ) ) {
-				form.classList.remove( 'search-form-visible' );
-				maiAriaClose( toggle );
-				document.removeEventListener( 'mouseup', hideSearchForm, true );
-			}
-		};
-
 		input.setAttribute( 'required', '' );
 
-		if ( ! ( target.classList.contains( 'search-form-input' ) || target.classList.contains( 'search-form-submit' ) || target.classList.contains( 'search-form' ) ) ) {
-			if ( form.classList.contains( 'search-form-visible' ) ) {
-				maiAriaClose( toggle );
-				form.classList.remove( 'search-form-visible' );
-				document.removeEventListener( 'mouseup', hideSearchForm, true );
-			} else {
-				maiAriaOpen( toggle );
-				form.classList.add( 'search-form-visible' );
-				input.focus();
-				document.addEventListener( 'mouseup', hideSearchForm, true );
-			}
+		maiToggleAriaValues( toggle );
+
+		form.classList.toggle( 'search-form-visible' );
+
+		if ( form.classList.contains( 'search-form-visible' ) ) {
+			body.addEventListener( 'keydown', maybeHideSearchForm, false );
+			body.addEventListener( 'click', maybeHideSearchForm, false );
+		} else {
+			body.removeEventListener( 'keydown', maybeHideSearchForm, false );
+			body.removeEventListener( 'click', maybeHideSearchForm, false );
 		}
 	};
 
-	// Sets scrollbar width on page load.
-	root.style.setProperty( '--scrollbar-width', scrollBarWidth + 'px' );
+	var maybeHideSearchForm = function( event ) {
+		if ( ! event.target.closest( '.search-icon-form' ) || [ 'Escape', 'Esc' ].includes( event.key ) ) {
+			var visibleSearchToggles = document.querySelectorAll( '.search-form-visible' );
+
+			visibleSearchToggles.forEach( function( searchToggle ) {
+				toggleSearchForm( searchToggle );
+			});
+		}
+	};
 
 	// Handles search toggle and form.
 	searchToggles.forEach( function( searchToggle ) {
-		searchToggle.addEventListener( 'click', toggleSearchForm, false );
+		searchToggle.addEventListener( 'click', toggleSearchFormEvent, false );
 	});
 } )();
-
-function maiAriaOpen( element ) {
-	element.setAttribute( 'aria-expanded', 'true' );
-	element.setAttribute( 'aria-pressed', 'true' );
-
-	return element;
-};
-
-function maiAriaClose( element ) {
-	element.setAttribute( 'aria-expanded', 'false' );
-	element.setAttribute( 'aria-pressed', 'false' );
-
-	return element;
-};
 
 function maiToggleAriaValues( element ) {
 	var ariaValue = element.getAttribute( 'aria-expanded' ) === 'false' ? 'true' : 'false';
