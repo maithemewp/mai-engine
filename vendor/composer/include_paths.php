a3005cea34f542aa071924ad94fa9fdacee205e8
Mike Hemberger <mike@thestizmedia.com>
2022-02-07 14:14:00 -0500

    update kirki to v4.0.19

--- vendor/composer/include_paths.php
+++ vendor/composer/include_paths.php
@@ -6,6 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    $vendorDir . '/aristath/kirki',
-    $vendorDir . '/aristath/kirki/core',
+    $vendorDir . '/kirki-framework/kirki',
+    $vendorDir . '/kirki-framework/kirki/core',
 );
