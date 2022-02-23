71e9c94213cdf0f9130f88a463a15942ff24920e
Mike Hemberger <mike@thestizmedia.com>
2022-02-23 16:55:43 -0500

    start pure

---
+++ config/pure.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Mai Engine.
+ *
+ * @package   BizBudding\MaiEngine
+ * @link      https://bizbudding.com
+ * @author    BizBudding
+ * @copyright Copyright © 2020 BizBudding
+ * @license   GPL-2.0-or-later
+ */
+
+return [
+	#f6f7f8
+	'global-styles' => [
+		'colors' => [
+			// 'black'      => '#000000',
+			// 'white'      => '#ffffff',
+			// 'header'     => '#ffffff', // Site header background.
+			// 'background' => '#ffffff', // Body background.
+			'alt'        => '#f9f9f9', // Background alt.Gray #f9f9f9. Blue #f8fbff. Tan #fbf8f5.
+			'body'       => '#111111', // Body text color.
+			'heading'    => '#111111', // Heading text color.
+			'link'       => '#02C091', // Link color.
+			'primary'    => '#02C091', // Button primary background color.
+			'secondary'  => '#737373', // Button secondary background color.
+		],
+		// Josefin Sans
+		'fonts' => [
+			'body'    => 'EB Garamond:400',
+			'heading' => 'Jost:600,400',
+		],
+	],
+	'image-sizes' => [
+		'add'    => [
+			'landscape' => '3:2',
+			'portrait'  => '2:3',
+		],
+	],
+	'theme-support' => [
+		'add' => [
+			'transparent-header',
+		],
+	],
+];
