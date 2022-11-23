041fd9bea521f06823676f812bc63fc2d7b2e361
Mike Hemberger <mike@thestizmedia.com>
2022-11-23 11:17:36 -0500

    second attempt at conditions fix
    for https://github.com/maithemewp/mai-engine/issues/624

--- lib/structure/template-parts.php
+++ lib/structure/template-parts.php
@@ -26,13 +26,12 @@ function mai_render_template_parts() {
 	foreach ( $template_parts as $slug => $template_part ) {
 		$hook      = isset( $template_part['hook'] ) ? $template_part['hook'] : false;
 		$priority  = isset( $template_part['priority'] ) ? $template_part['priority'] : 10;
-		$condition = isset( $template_part['condition'] ) ? $template_part['condition'] : '';
 		$before    = isset( $template_part['before'] ) ? $template_part['before'] : '';
 		$after     = isset( $template_part['after'] ) ? $template_part['after'] : '';
 
 		if ( $hook && ! mai_is_element_hidden( mai_convert_case( $slug ) ) ) {
-			add_action( $hook, function() use ( $slug, $before, $after, $condition ) {
-				mai_render_template_part( $slug, $before, $after, $condition );
+			add_action( $hook, function() use ( $slug, $before, $after ) {
+				mai_render_template_part( $slug, $before, $after );
 			}, $priority );
 		}
 	}
