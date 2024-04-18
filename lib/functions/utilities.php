f2b71f44e19ae9b45c0dc32daa565bd61a12c88c
JiveDig <jivetheuniversal@gmail.com>
2024-04-18 14:08:03 -0400

    Change order of `mai_get_processed_content()` to match `get_the_block_template_html()`

--- lib/functions/utilities.php
+++ lib/functions/utilities.php
@@ -601,9 +601,10 @@ function mai_get_post_content( $post_slug_or_id, $post_type = 'wp_block' ) {
  *
  * Most of the order comes from /wp-includes/default-filters.php.
  *
- * @since 2.19.0 Conditionally `do_blocks()` or `wpautop()`.
- * @since 2.4.2  Remove use of wp_make_content_images_responsive.
  * @since 0.3.0
+ * @since 2.4.2  Remove use of wp_make_content_images_responsive.
+ * @since 2.19.0 Conditionally `do_blocks()` or `wpautop()`.
+ * @since TBD Changed order to match `get_the_block_template_html()`.
  *
  * @param string $content The unprocessed content.
  *
@@ -618,15 +619,15 @@ function mai_get_processed_content( $content ) {
 	global $wp_embed;
 
 	$blocks  = has_blocks( $content );
-	$content = $wp_embed->autoembed( $content );           // WP runs priority 8.
-	$content = $wp_embed->run_shortcode( $content );       // WP runs priority 8.
-	$content = $blocks ? do_blocks( $content ) : $content; // WP runs priority 9.
-	$content = wptexturize( $content );                    // WP runs priority 10.
-	$content = ! $blocks ? wpautop( $content ) : $content; // WP runs priority 10.
-	$content = shortcode_unautop( $content );              // WP runs priority 10.
-	$content = function_exists( 'wp_filter_content_tags' ) ? wp_filter_content_tags( $content ) : wp_make_content_images_responsive( $content ); // WP runs priority 10. WP 5.5 with fallback.
-	$content = do_shortcode( $content );                   // WP runs priority 11.
-	$content = convert_smilies( $content );                // WP runs priority 20.
+	$content = $wp_embed->run_shortcode( $content );
+	$content = $wp_embed->autoembed( $content );
+	$content = shortcode_unautop( $content );
+	$content = do_shortcode( $content );
+	$content = $blocks ? do_blocks( $content ) : $content;
+	$content = wptexturize( $content );
+	$content = convert_smilies( $content );
+	$content = wp_filter_content_tags( $content, 'template' );
+	$content = str_replace( ']]>', ']]&gt;', $content );
 
 	return $content;
 }
