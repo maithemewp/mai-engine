ca27954fe2b20b27a04cbe822941a5501986134e
JiveDig <jivetheuniversal@gmail.com>
2024-07-18 17:26:41 -0400

    Remove unnecessary nonce check

--- lib/fields/functions.php
+++ lib/fields/functions.php
@@ -16,13 +16,14 @@ defined( 'ABSPATH' ) || die;
  * Gets an ACF request, checking nonce and value.
  *
  * @since 0.1.0
+ * @since TBD Remove unnecessary nonce check.
  *
  * @param string $request Request data.
  *
  * @return bool
  */
 function mai_get_acf_request( $request ) {
-	if ( isset( $_REQUEST['nonce'] ) && wp_verify_nonce( $_REQUEST['nonce'], 'acf_nonce' ) && isset( $_REQUEST[ $request ] ) && ! empty( $_REQUEST[ $request ] ) ) {
+	if ( isset( $_REQUEST[ $request ] ) && ! empty( $_REQUEST[ $request ] ) ) {
 		return $_REQUEST[ $request ];
 	}
 
