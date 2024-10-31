7ddcb2e7235ee49a0146bb6c4fd1eeed14fa9d67
JiveDig <jivetheuniversal@gmail.com>
2024-10-30 21:12:32 -0400

    Update gulp to v5

--- bin/i18n.js
+++ bin/i18n.js
@@ -1,5 +1,5 @@
 'use strict';
 
-module.exports = function() {
+module.exports = function i18nTask() {
 	return require( 'child_process' ).exec( 'composer i18n' );
 };
--- bin/images.js
+++ bin/images.js
@@ -1,39 +1,43 @@
 'use strict';
 
-const gulp     = require( 'gulp' ),
-	  config   = require( './config' ),
-	  changed  = require( 'gulp-changed' ),
-	  filter   = require( 'gulp-filter' ),
-	  imagemin = require( 'gulp-imagemin' ),
-	  notify   = require( 'gulp-notify' ),
-	  map      = require( 'lodash.map' );
+const gulp = require('gulp');
+const config = require('./config');
+const notify = require('gulp-notify');
+const map = require('lodash.map');
 
-module.exports.img = function() {
-	const allButScreenshot = filter( [ '**/*', '!**/screenshot.*' ], { restore: true } );
-	const onlyScreenshot   = filter( [ '**/screenshot.*' ] );
+// Named function for image processing
+module.exports.img = async function imgTask() {
+    // Dynamic imports for ES Modules
+    const changed = (await import('gulp-changed')).default;
+    const filter = (await import('gulp-filter')).default;
+    const imagemin = (await import('gulp-imagemin')).default;
 
-	return gulp.src( config.src.images )
-		.pipe( changed( config.dest.images ) )
-		.pipe( imagemin( {
-			verbose: true,
-			optimizationLevel: 3,
-			progressive: true,
-			interlaced: true
-		} ) )
-		.pipe( allButScreenshot )
-		.pipe( gulp.dest( config.dest.images ) )
-		.pipe( allButScreenshot.restore )
-		.pipe( onlyScreenshot )
-		.pipe( gulp.dest( './' ) )
-		.pipe( notify( { message: config.messages.images } ) );
+    const allButScreenshot = filter(['**/*', '!**/screenshot.*'], { restore: true });
+    const onlyScreenshot = filter(['**/screenshot.*']);
+
+    return gulp.src(config.src.images)
+        .pipe(changed(config.dest.images))
+        .pipe(imagemin({
+            verbose: true,
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(allButScreenshot)
+        .pipe(gulp.dest(config.dest.images))
+        .pipe(allButScreenshot.restore)
+        .pipe(onlyScreenshot)
+        .pipe(gulp.dest('./'))
+        .pipe(notify({ message: config.messages.images }));
 };
 
-module.exports.svg = function() {
-	return map( config.src.svg, function( style ) {
-		const dir = style.split( '/' );
+// Named function for SVG processing
+module.exports.svg = function svgTask() {
+    return Promise.all(map(config.src.svg, function (style) {
+        const dir = style.split('/');
 
-		return gulp.src( style + '/*' )
-			.pipe( gulp.dest( config.dest.svg + '/' + dir[ dir.length - 1 ] ) )
-			.pipe( notify( { message: config.messages.svg } ) );
-	} );
+        return gulp.src(style + '/*')
+            .pipe(gulp.dest(config.dest.svg + '/' + dir[dir.length - 1]))
+            .pipe(notify({ message: config.messages.svg }));
+    }));
 };
