3ac8e5782eeb6b991b300cbfab84e26eed9276c8
JiveDig <jivetheuniversal@gmail.com>
2025-10-20 15:14:30 -0400

    2.38.0-beta.3

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'maithemewp/mai-engine',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => '229b892e26a11f0378e202aaabacd144bf63b0c2',
+        'reference' => 'e78c10a68f0ac2e776d9b1dc69ee5f9cfcaf71de',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -40,7 +40,7 @@
         'maithemewp/mai-engine' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => '229b892e26a11f0378e202aaabacd144bf63b0c2',
+            'reference' => 'e78c10a68f0ac2e776d9b1dc69ee5f9cfcaf71de',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -58,18 +58,18 @@
         'wpackagist-plugin/advanced-custom-fields' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '6.6.0',
+                0 => '6.6.1',
             ),
         ),
         'wpengine/advanced-custom-fields' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => '6.6.0',
+                0 => '6.6.1',
             ),
         ),
         'wpengine/advanced-custom-fields-pro' => array(
-            'pretty_version' => '6.6.0',
-            'version' => '6.6.0.0',
+            'pretty_version' => '6.6.1',
+            'version' => '6.6.1.0',
             'reference' => null,
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../wpengine/advanced-custom-fields-pro',
